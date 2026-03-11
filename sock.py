a8eafd19e8b4170571f68c91906ea3bb8e519f31
baiemir <baemir.omurzak0v@mail.ru>
2026-03-11 22:47:21 +0600

    Delete while cycle in liste function

--- sock.py
+++ sock.py
@@ -4,21 +4,22 @@ import socket
 def echo(connection: socket.socket) -> None | str:
     """Connection handler"""
     while data := connection.recv(1024):
-        if data == b'exit\r\n':
-            break
-        print(f"Полученное сообщение {data}")
-        message = b"your message is: " + data
-        connection.sendall(message)
-    raise Exception
-
-def listen_for_connection(server_socket: socket.socket):
-    """Listens for incoming connections"""
-    while True:
         try:
-            connection, address = server_socket.accept()
-            print(f"{address} connected")
-            echo(connection)
-        except Exception:
-            print("Connection closed")
-            break
-        
+            if data == b'exit\r\n':
+                print("Connection closed")
+                connection.close()
+            print(f"Полученное сообщение {data}")
+            message = b"your message is: " + data
+            connection.sendall(message)
+        except Exception as e:
+            print(e)
+
+def listen_for_connection(server_socket: socket.socket) -> None:
+    """Listens for incoming connections"""
+    try:
+        connection, address = server_socket.accept()
+        print(f"{address} connected")
+        echo(connection)
+    except Exception:
+        print("Connection closed")
+    
