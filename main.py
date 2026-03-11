acdd00050b2a8750ee8029b3951537dad41ee975
baiemir <baemir.omurzak0v@mail.ru>
2026-03-11 22:20:00 +0600

    split 'echo' and 'list' funcitions separete modules

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
 import socket
+from sock import echo, listen_for_connection
+from pathlib import Path
 
 
+path = Path(__file__)
 HOST = '127.0.0.1'
 PORT = 8000
-
-def echo(connection: socket.socket) -> None:
-    while data := connection.recv(1024):
-        connection.sendall(data)
-
-def listen_for_connection(server_socket: socket.socket):
-    while True:
-        connection, address = server_socket.accept()
-        print(f"{address} connected")
-        echo(connection)
-
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -25,7 +17,6 @@ def main():
     print(f"server address: {server_addres}")
     server_socket.listen(1)
     listen_for_connection(server_socket)
-
 
 if __name__ == "__main__":
     main()
